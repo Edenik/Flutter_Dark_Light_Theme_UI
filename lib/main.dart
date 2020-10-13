b83432a4a156dc140f44ab557237c7c46766ec32
Edenik <Edenik5@gmail.com>
2020-10-13 13:50:23 +0300

    implementing dark/light switch mode with shared preferences

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,20 +8,123 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Dynamic Theme Switcher',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dynamic Theme Switcher'),
-        ),
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return DarkLightTheme();
+  }
+}
+
+class DarkLightTheme extends StatefulWidget {
+  const DarkLightTheme({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _DarkLightThemeState createState() => _DarkLightThemeState();
+}
+
+ThemeData _lightTheme = ThemeData(
+  accentColor: Colors.purple,
+  brightness: Brightness.light,
+  primaryColor: Colors.green,
+  buttonTheme: ButtonThemeData(
+    buttonColor: Colors.green,
+    textTheme: ButtonTextTheme.primary,
+  ),
+);
+
+ThemeData _darkTheme = ThemeData(
+  accentColor: Colors.amber[800],
+  brightness: Brightness.dark,
+  primaryColor: Colors.amber,
+  buttonTheme: ButtonThemeData(
+    buttonColor: Colors.amber,
+    textTheme: ButtonTextTheme.primary,
+  ),
+);
+
+bool _light = true;
+Future<bool> _lightF;
+
+class _DarkLightThemeState extends State<DarkLightTheme> {
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  _saveTheme() async {
+    SharedPreferences pref = await _prefs;
+    pref.setBool('theme', _light);
+  }
+
+  _getTheme() async {
+    _lightF = _prefs.then((SharedPreferences prefs) {
+      return prefs.getBool('theme') != null ? prefs.getBool('theme') : true;
+    });
+    _light = await _lightF;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getTheme();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _lightF,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return MaterialApp(
+              home: Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            );
+          }
+          return MaterialApp(
+            title: 'Dynamic Theme Switcher',
+            debugShowCheckedModeBanner: false,
+            theme: _light ? _lightTheme : _darkTheme,
+            darkTheme: _darkTheme,
+            home: Scaffold(
+              appBar: AppBar(
+                title: Text('Dynamic Theme Switcher'),
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(
+                        labelText: "Enter Name",
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Text('Click Me'),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Text('Switch Theme'),
+                    Switch(
+                      inactiveThumbColor: _light
+                          ? _lightTheme.accentColor
+                          : _darkTheme.accentColor,
+                      inactiveTrackColor: _light
+                          ? _lightTheme.accentColor
+                          : _darkTheme.primaryColor,
+                      value: _light,
+                      onChanged: (state) {
+                        setState(() {
+                          _light = state;
+                        });
+                        _saveTheme();
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
   }
 }

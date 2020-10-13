34d19c7711428d2e6e7a4cb97e181a3aa72fb8ee
Edenik <Edenik5@gmail.com>
2020-10-13 12:56:57 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Flutter Dark Light Theme UI 🌗
+
+Tutorial by Mighty Techno -
+https://mightytechno.com/dark-light-theme-flutter-app/
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dynamic Theme Switcher',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Dynamic Theme Switcher'),
+        ),
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
